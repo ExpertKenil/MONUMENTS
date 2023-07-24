9f4ef4e18b2e311a4dcdd02a8c5e9fce5cb69de3
Expert_Kenil <106904637+ExpertKenil@users.noreply.github.com>
2023-07-24 21:14:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let latitude = 22.7868542, longitude = 88.3643296;
 
-// Initializing Mapbox
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
 
 var map = new mapboxgl.Map({
@@ -20,7 +19,6 @@ map.addControl(
 
 var img1 = document.querySelector("#amber")
 
-// Create a Amber Palace, Jaipur Marker and add it to the map.
 var marker1 = new mapboxgl.Marker({
 	element: img1
 })
@@ -28,7 +26,7 @@ var marker1 = new mapboxgl.Marker({
 	.addTo(map);
 
 var img2 = document.querySelector("#gateway")
-// Create a  Gateway of India, Mumbai Marker and add it to the map.
+
 var marker2 = new mapboxgl.Marker({
 	element: img2
 })
@@ -36,7 +34,7 @@ var marker2 = new mapboxgl.Marker({
 	.addTo(map);
 
 var img3 = document.querySelector("#gate")
-// Create a India Gate Marker and add it to the map.
+
 var marker3 = new mapboxgl.Marker({
 	element: img3
 })
@@ -46,7 +44,7 @@ var marker3 = new mapboxgl.Marker({
 
 var img4 = document.querySelector("#lotus")
 
-// Create a Lotus Temple, Delhi Marker and add it to the map.
+
 var marker4 = new mapboxgl.Marker({
 	element: img4
 })
@@ -54,7 +52,7 @@ var marker4 = new mapboxgl.Marker({
 	.addTo(map);
 
 
-//Create a Victoria Memorial, Kolkata Marker and add it to the map.
+
 var img5 = document.querySelector("#victoria")
 
 var marker5 = new mapboxgl.Marker({
